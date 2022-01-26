d69f4c8dcbde97b45abc5747df5b54e2982f0263
azamudinreldyn <azamudin@reldyn.co>
2022-01-26 18:57:20 +0800

    latest-commit

--- app/src/main/java/com/example/androidcolorwheel/MainFragment.kt
+++ app/src/main/java/com/example/androidcolorwheel/MainFragment.kt
@@ -1,5 +1,6 @@
 package com.example.androidcolorwheel
 
+import android.content.res.ColorStateList
 import android.graphics.Color
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
@@ -14,6 +15,10 @@ import com.github.antonpopoff.colorwheel.ColorWheel
 class MainFragment : Fragment(), View.OnClickListener {
 
     lateinit var binding: MainFragmentBinding
+    private var color1:Int = Color.parseColor("#00c2a3")
+    private var color2:Int = Color.parseColor("#4ba54f")
+    private var color3:Int = Color.parseColor("#ff6100")
+
 
     companion object {
         fun newInstance() = MainFragment()
@@ -32,46 +37,61 @@ class MainFragment : Fragment(), View.OnClickListener {
 
         binding.colorWheel.colorChangeListener = { rgb: Int ->
             binding.tvRgb.text = "Selected RGB : $rgb"
+
+            when {
+                binding.card1.isActivated -> {
+                    binding.view1.backgroundTintList = ColorStateList.valueOf(rgb)
+                    color1 = rgb
+                }
+                binding.card2.isActivated -> {
+                    binding.view2.backgroundTintList = ColorStateList.valueOf(rgb)
+                    color2 = rgb
+                }
+                binding.card3.isActivated -> {
+                    binding.view3.backgroundTintList = ColorStateList.valueOf(rgb)
+                    color3 = rgb
+                }
+            }
+
         }
 
-        binding.cardTeal.setOnClickListener(this)
+        binding.card1.setOnClickListener(this)
 
-        binding.cardGreen.setOnClickListener(this)
+        binding.card2.setOnClickListener(this)
 
-        binding.cardOrange.setOnClickListener(this)
+        binding.card3.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
         when (v!!.id) {
-            binding.cardTeal.id -> {
-                binding.cardTeal.isActivated = !binding.cardTeal.isActivated
-                binding.cardGreen.isActivated = false
-                binding.cardOrange.isActivated = false
+            binding.card1.id -> {
+                binding.card1.isActivated = !binding.card1.isActivated
+                binding.card2.isActivated = false
+                binding.card3.isActivated = false
 
-                setColor(R.color.teal)
+                setColor(color1)
             }
-            binding.cardGreen.id -> {
-                binding.cardGreen.isActivated = !binding.cardGreen.isActivated
-                binding.cardTeal.isActivated = false
-                binding.cardOrange.isActivated = false
+            binding.card2.id -> {
+                binding.card2.isActivated = !binding.card2.isActivated
+                binding.card1.isActivated = false
+                binding.card3.isActivated = false
 
-                setColor(R.color.green)
+                setColor(color2)
             }
-            binding.cardOrange.id -> {
-                binding.cardOrange.isActivated = !binding.cardOrange.isActivated
-                binding.cardGreen.isActivated = false
-                binding.cardTeal.isActivated = false
+            binding.card3.id -> {
+                binding.card3.isActivated = !binding.card3.isActivated
+                binding.card2.isActivated = false
+                binding.card1.isActivated = false
 
-                setColor(R.color.orange)
+                setColor(color3)
             }
         }
     }
 
     private fun setColor(int: Int) {
-        val color = resources.getColor(int)
-        val r: Int = Color.red(color)
-        val g: Int = Color.green(color)
-        val b: Int = Color.blue(color)
+        val r: Int = int shr 16 and 0xFF
+        val g: Int = int shr 8 and 0xFF
+        val b: Int = int and 0xFF
 
         binding.colorWheel.setRgb(r,g,b)
     }
